4d5c442602737ff534cba8a8d284ecf4638179a8
Jamie Johnstone <j@miej.co>
2020-09-09 14:33:12 +0100

    Implement DomesticCat with isAsleep() and goToSleep()

--- src/main/java/com/techreturners/cats/Cat.java
+++ src/main/java/com/techreturners/cats/Cat.java
@@ -1,5 +1,21 @@
 package com.techreturners.cats;
 
 interface Cat {
+    public void eat();
+    public void goToSleep();
+    public void wakeUp();
+    public Boolean isAsleep();
+}
 
+class DomesticCat implements Cat {
+    private Boolean asleep = false;
+    private Integer height = 22;
+
+    public Boolean isAsleep() {
+        return asleep;
+    }
+
+    public void goToSleep() {
+        this.asleep = true;
+    }
 }
