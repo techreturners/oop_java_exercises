c1981e873f7d934d81ca0d1b0c95469346ab28ed
Jamie Johnstone <j@miej.co>
2020-09-09 17:41:30 +0100

    Implement LionCat() class

--- src/main/java/com/techreturners/cats/Cat.java
+++ src/main/java/com/techreturners/cats/Cat.java
@@ -1,7 +1,6 @@
 package com.techreturners.cats;
 
 interface Cat {
-    public void eat();
     public void goToSleep();
     public void wakeUp();
     public Boolean isAsleep();
@@ -13,6 +12,32 @@ class DomesticCat implements Cat {
     private Boolean asleep = false;
     private Integer height = 23;
     private String setting = "domestic";
+
+    public Boolean isAsleep() {
+        return asleep;
+    }
+
+    public void goToSleep() {
+        this.asleep = true;
+    }
+
+    public void wakeUp() {
+        this.asleep = false;
+    }
+
+    public String getSetting() {
+        return setting;
+    }
+
+    public Integer getAverageHeight() {
+        return height;
+    }
+}
+
+class LionCat implements Cat {
+    private Boolean asleep = false;
+    private Integer height = 1100;
+    private String setting = "wild";
 
     public Boolean isAsleep() {
         return asleep;
